14d11159f2e96b5b0848f4a651390d7227a555de
Zhaolong <zztg2@mail.missouri.edu>
2014-04-11 17:05:35 -0500

    Added volunteer link to home_view

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,1 +1,5 @@
 <a href="<?php echo base_url();?>login/do_logout/">Logout</a> <a>| Welcome <?php echo $this->session->userdata('username');?></a>
+<div>
+<a href="<?php echo base_url();?>volunteer">Volunteer Page</a>
+
+</div>
