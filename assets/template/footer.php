bf0db30cc57e9fe53e6d708a344a3a78c46245bd
Zhaolong <zztg2@mail.missouri.edu>
2014-04-11 13:22:19 -0500

    Added heade.php and footer.php to assets/template.

---
+++ assets/template/footer.php
@@ -0,0 +1,26 @@
+ <footer>
+        <div class="row">
+          <div class="col-lg-12">
+
+            <ul class="list-unstyled">
+              <li class="pull-right"><a href="">Back to top</a></li>
+              <li><a href="" onclick="pageTracker._link(this.href); return false;">Blog</a></li>
+              <li><a href="">RSS</a></li>
+              <li><a href="">Twitter</a></li>
+            </ul>
+            <p>Made by <a href="" rel="nofollow">LEAD</a></P>
+          </div>
+        </div>
+
+      </footer>
+
+
+    </div>
+
+
+    <script src="./assets/js/jquery-1.10.2.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <script src="./assets/js/bootswatch.js"></script>
+  
+
+</body></html>
---
+++ assets/template/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>LEAD</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./assets/css/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="./assets/css/bootswatch.min.css">
+    
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <img href="" class="navbar-brand" src="../img/LEADLogo.png">
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="" id="themes">Themes <span class="caret"></span></a>
+              <ul class="dropdown-menu" aria-labelledby="themes">
+                <li><a href="">Default</a></li>
+                <li class="divider"></li>
+                <li><a href="">Link</a></li>
+                <li><a href="">Link</a></li>
+                <li><a href="">Link</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="">Help</a>
+            </li>
+            <li>
+              <a href="">Blog</a>
+            </li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="" id="download">Download <span class="caret"></span></a>
+              <ul class="dropdown-menu" aria-labelledby="download">
+                <li><a href="">Link</a></li>
+                <li><a href="">Link</a></li>
+                <li class="divider"></li>
+                <li><a href="">Link</a></li>
+                <li><a href="">Link</a></li>
+              </ul>
+            </li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="" target="_blank">Link</a></li>
+            <li><a href="" target="_blank">Link</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container">
