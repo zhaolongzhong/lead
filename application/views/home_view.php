006e375b565e685e8743bc3cd8e72ca56345484a
Zhaolong <zztg2@mail.missouri.edu>
2014-04-13 16:57:37 -0500

    Add account page link to it.

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,5 +1,10 @@
 <a href="<?php echo base_url();?>login/do_logout/">Logout</a> <a>| Welcome <?php echo $this->session->userdata('username');?></a>
 <div>
+
 <a href="<?php echo base_url();?>volunteer">Volunteer Page</a>
+<a href="<?php echo base_url();?>admin">Admin Page</a>
+<a href="<?php echo base_url();?>systemlog">System Log</a>
+<a href="<?php echo base_url();?>account">Account Page</a>
+<a href="<?php echo base_url();?>test">Test Page</a>
 
 </div>
