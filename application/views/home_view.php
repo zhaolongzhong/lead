d34fa222b9c39f268d814927e33ca8d3fb3ea4c1
Zhaolong <zztg2@mail.missouri.edu>
2014-04-13 21:52:49 -0500

    Added header and footer to home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,3 +1,4 @@
+<?php include './assets/template/header.php'; ?>
 <a href="<?php echo base_url();?>login/do_logout/">Logout</a> <a>| Welcome <?php echo $this->session->userdata('username');?></a>
 <div>
 
@@ -8,3 +9,4 @@
 <a href="<?php echo base_url();?>test">Test Page</a>
 
 </div>
+<?php include './assets/template/footer.php'; ?>
