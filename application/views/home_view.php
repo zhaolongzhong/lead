2110e4f4b3d04a9833f5cc75c3dc5f1c0717de16
Zhaolong <zztg2@mail.missouri.edu>
2014-04-13 22:08:50 -0500

    Edited the layout of home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,12 +1,12 @@
 <?php include './assets/template/header.php'; ?>
-<a href="<?php echo base_url();?>login/do_logout/">Logout</a> <a>| Welcome <?php echo $this->session->userdata('username');?></a>
+<h2>Welcome <?php echo $this->session->userdata('username');?></h2>
 <div>
 
-<a href="<?php echo base_url();?>volunteer">Volunteer Page</a>
-<a href="<?php echo base_url();?>admin">Admin Page</a>
-<a href="<?php echo base_url();?>systemlog">System Log</a>
-<a href="<?php echo base_url();?>account">Account Page</a>
-<a href="<?php echo base_url();?>test">Test Page</a>
+<a href="<?php echo base_url();?>volunteer">Volunteer Page</a><br />
+<a href="<?php echo base_url();?>admin">Admin Page</a><br />
+<a href="<?php echo base_url();?>systemlog">System Log</a><br />
+<a href="<?php echo base_url();?>account">Account Page</a><br />
+<a href="<?php echo base_url();?>test">Test Page</a><br />
 
 </div>
 <?php include './assets/template/footer.php'; ?>
